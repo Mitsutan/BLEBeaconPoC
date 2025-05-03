520a8288aa78caf2cddd418d84c06ee2a5eb9cbb
Mitsutan <83488293+Mitsutan@users.noreply.github.com>
2025-05-03 20:29:20 +0900

    Initial Commit

---
+++ BLEBeaconPoC/BLEBeaconPoCApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEBeaconPoCApp.swift
+//  BLEBeaconPoC
+//
+//  Created by Mitsutan on 2025/05/03.
+//
+
+import SwiftUI
+
+@main
+struct BLEBeaconPoCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

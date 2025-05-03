731b2a75c280365c4ad73273d92b406e76d49dbb
Mitsutan <83488293+Mitsutan@users.noreply.github.com>
2025-05-03 22:46:11 +0900

    Update ContentView.swift
    とりあえずの画面

--- BLEBeaconPoC/ContentView.swift
+++ BLEBeaconPoC/ContentView.swift
@@ -8,12 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject private var beaconScanner = BeaconScanner()
+    @State private var isAdvertising: Bool = false
+    
+    private let beaconAdvertisement = BeaconAdvertisement()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("スキャン状態:" + (beaconScanner.isScanning ? "ON" : "OFF"))
+            Text("アドバタイズ状態:" + (isAdvertising ? "ON" : "OFF"))
+            
+            Button(action: {
+                isAdvertising = beaconAdvertisement.startAdvertising(userId: UUID.init().uuidString)
+            }) {
+                Text("アドバタイズ")
+                    .padding()
+            }
         }
         .padding()
     }
